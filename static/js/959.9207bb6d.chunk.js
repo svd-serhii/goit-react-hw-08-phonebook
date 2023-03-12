53c35a4425458951c2be507bab44cffe46e3ed57
svd-serhii <107747926+svd-serhii@users.noreply.github.com>
2023-03-12 20:43:01 +0000

    Deploying to gh-pages from @ svd-serhii/goit-react-hw-08-phonebook@e63f602fb8d8e63da317c7aee5174c46546b2e46 🚀

---
+++ static/js/959.9207bb6d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[959],{4959:function(e,n,o){o.r(n),o.d(n,{default:function(){return h}});o(2791);var t="welcome_title__fzMUZ",c=o(184),h=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{className:t,children:"Phonebook App"})})}}}]);
+//# sourceMappingURL=959.9207bb6d.chunk.js.map
